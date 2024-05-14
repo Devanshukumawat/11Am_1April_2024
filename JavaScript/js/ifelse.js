3b43ceebc3c031af460b7e46fbca059fdbbdd59b
Devanshukumawat <devanshukumawat063@gmail.com>
2024-05-14 12:04:49 +0530

    add files

--- JavaScript/js/ifelse.js
+++ JavaScript/js/ifelse.js
@@ -1,16 +1,16 @@
 const prompt = require ("prompt-sync")()
 
-const age = prompt("Enter Age :- ")
+// const age = prompt("Enter Age :- ")
 
-if(age<18){
-    console.log("You are not Eligible for vote")
-}
-else if(age>=18){
-    console.log("You are Eligible for vote")
-}
-else{
-    console.log("Invalid input")
-}
+// if(age<18){
+//     console.log("You are not Eligible for vote")
+// }
+// else if(age>=18){
+//     console.log("You are Eligible for vote")
+// }
+// else{
+//     console.log("Invalid input")
+// }
 
 
 // npm init -y
@@ -23,3 +23,71 @@ else{
 //     Subtraction
 //     Multiplication
 //     Division
+
+
+
+// npm init -y   -> package.json
+// npm i/install prompt-sync   -> package install
+// require("prompt-sync")()  -> call
+
+
+// const prompt = require ("prompt-sync")()
+
+// const num1 = parseInt( prompt("Enter Number (1) :- "))
+// const num2 = parseInt(prompt("Enter Number (2) :- "))
+// const operator = prompt("Enter Operator (+,-,/,*) :- ")
+
+// let result = ''
+
+// if(operator=="+"){
+//     result = num1+num2   // "7"+"7" = "77" -> 7 + 7 = 14
+// }
+// else if(operator == "-"){
+//     result = num1-num2  // "7"-"7" = "0"
+// }
+// else if(operator=="*"){
+//     result = num1*num2  // "7"*"7" = "49"
+// }
+// else if (operator=="/"){
+//     result = num1/num2  // "7"/"7" = "1"
+// } 
+
+// console.log(` Calculation :- ${num1} ${operator} ${num2} = ${result} `)
+
+
+// let firstName = "Devanshu"
+
+// console.log("hello "+firstName)
+// console.log(` Hello ${firstName} `)
+
+
+// let numbers = 4 //-> even
+// let numbers = 7 //-> odd
+
+
+// const num = prompt("Enter Number :- ")
+
+// if(num%2==0){
+//     console.log("Even Number")
+// }else{
+//     console.log("Odd Number")
+// }
+
+
+// Find the grade for input marks
+//      "A+ grade" if marks is between 90 and 100.
+//      "A grade" if marks is between 80 and 90.
+//      "B+ grade" if marks is between 70 and 80.
+//      "B grade" if marks is between 60 and 70.
+//      "C grade" if marks is between 45 and 60.
+//      "D grade" if marks is between 35 and 45.
+//      "Fail" if marks is below 35  
+
+
+
+
+
+
+
+
+
