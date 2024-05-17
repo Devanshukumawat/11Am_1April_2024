a540f8c75cfe80623457cec2c63ecb257cf956c6
DevanshuKumawat <devanshukumawat063@gmail.com>
2024-05-17 11:25:18 +0530

    ADD FILES

--- JavaScript/js/ifelse.js
+++ JavaScript/js/ifelse.js
@@ -83,6 +83,103 @@ const prompt = require ("prompt-sync")()
 //      "D grade" if marks is between 35 and 45.
 //      "Fail" if marks is below 35  
 
+// let age = 10
+// let num = 90
+
+// if(age == 10){
+//     if(num >10){
+//         console.log("Number is grater then 10")
+//     }else{
+//         console.log("Number is not grater then 10")
+//     }
+// }
+// else if(age==20){
+//     if(num <10){
+//         console.log("Number is less then 10")
+//     }else{
+//         console.log("Number is not less then 10")
+//     }
+// }
+// else{
+//     console.log("number is not equal to 10")
+// }
+
+
+// const height = prompt("Enter Height :-")
+
+// if(height<1){
+//     const squaek = prompt("Cannot squeak / Can Squeak (yes/no):- ")
+//     if(squaek=="yes"){
+//         console.log("Might be a rat")
+//     }else if(squaek=="no"){
+//         console.log("Might be a squirrel")
+//     }else{
+//         console.log("Invalid")
+//     }
+// }
+// else if(height>=1){
+//     const neck = prompt("Short neck / Long neck (short/long):- ")
+//     if(neck=="short"){
+//         const nose = prompt("Short Nose / Long Nose (short/long):- ")
+//         if(nose=="short"){
+//            const stay =  prompt("On land / In water (land/water):- ")
+//            if(stay=="water"){
+//             console.log("Might be a hippo")
+//            }
+//            else if(stay=="land"){
+//             console.log("Might be rhincorce")
+//            }
+//            else{
+//             console.log("Invalid")
+//         }
+           
+//         }
+//         else if(nose=="long"){
+//             console.log("Might be a elephant")
+//         }
+//         else{
+//             console.log("Invalid")
+//         }
+//     }else if(neck=="long"){
+//         console.log("Might be a Giraffe")
+//     }
+//     else{
+//         console.log("Invalid")
+//     }
+// }
+// else{
+//     console.log("Invalid Height")
+// }
+
+
+//1-10
+
+// 1-50  1-100
+
+// user1 = 30
+// user2 = 35
+
+const num1 = prompt.hide("Enter Number {1-10} (User-1) :- ")
+const num2 = prompt("Enter Number {1-10} (user-2) :- ")
+
+if(num1==num2){
+    console.log(`user1:-  ${num1} user2:- ${num2} result :- Yahhooo... You won.. 🥳 `)
+}else{
+    console.log(`user1:- ${num1} user2:-  ${num2} result :- Better Luck next time ..😕 `)
+}
+
+/*
+
+
+user input = ""
+Devanshu = Dev
+Dev = Devanshu
+Kizie = Manny
+Manny = Kizie
+
+
+
+*/
 
 
 
