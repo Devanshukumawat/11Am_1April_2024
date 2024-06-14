129b5049e9653116aa8c19a94faf11f4825b29f9
DevanshuKumawat <devanshukumawat063@gmail.com>
2024-06-14 12:26:31 +0530

    add files

--- JavaScript/js/promise.js
+++ JavaScript/js/promise.js
@@ -6,14 +6,13 @@
 //     setTimeout(()=>{
 //         console.log("Promise Created....Async Task complete...")
 //         reject()
-        
+
 //     },2000)
 // }).then((res)=>{
 //     console.log("Hello")
 // }).catch((error)=>{
 //     console.log("Error")
 // })
-
 
 // let myPromise = new Promise((resolve,reject)=>{
 //     setTimeout(()=>{
@@ -31,7 +30,6 @@
 //     console.log(res.name)
 // })
 
-
 // myPromise.catch((error)=>{
 //     console.log(error)
 // })
@@ -40,13 +38,47 @@
 //     console.log("Always Run finally")
 // })
 
+// fetch("https://dummyjson.com/products").then((res)=>{
+//     return res.json()
+// }).then((data)=>{
+//     console.log(data)
+// })
+// .catch((error)=>{
+//     console.log(error)
+// })
 
+// let myPromise = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log("Async Task....")
+//         let task = false
+//         if(task){
+//             resolve({name:"Devanshu",age:34})
+//         }else{
+//             reject({Error:"Kuch to gadbad hai"})
+//         }
+//     },2000)
+// })
 
-fetch("https://dummyjson.com/products").then((res)=>{
-    return res.json()
-}).then((data)=>{
-    console.log(data)
-})
-.catch((error)=>{
-    console.log(error)
-})
+// async function Add(){
+
+//     try {
+//         let data = await myPromise
+//     console.log(data)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// Add()
+
+async function data() {
+  try {
+    let product = await fetch("https://dummyjson.com/products/300");
+    const data = await product.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+data();
