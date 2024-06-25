70c48553ac32c294ce1ef1ce369a8e2fc9b0a051
DevanshuKumawat <devanshukumawat063@gmail.com>
2024-06-25 12:37:55 +0530

    add

---
+++ JavaScript/js/logical.js
@@ -0,0 +1,33 @@
+
+
+// && = true true = true
+//      true false = false
+//      false true = false
+//      false false = false
+
+// || = true true = true
+//      false true = true 
+//      true false = true 
+//      false false = false
+
+
+// let firstCondition = true
+// let secondCondition = true
+
+// if(firstCondition&&secondCondition){
+//     console.log("True")
+// }else{
+//     console.log("False")
+// }
+
+// let firstCondition = false
+// let secondCondition = false
+
+// if(firstCondition||secondCondition){
+//     console.log("True")
+// }else{
+//     console.log("False")
+// }
+
+
+// console.log(2!=6)
---
+++ JavaScript/js/loop.js
@@ -0,0 +1,132 @@
+// for(let i = 1; i<=8; i++){
+//     console.log(i)
+// }
+
+// let myArray = ["dev","aman","kizie","manny"]
+
+// for(let key in myArray){
+//     console.log(key)
+//      console.log(myArray[key])
+// }
+
+// let i = 1
+
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+// prompt = input = 2/3/4 = table
+
+
+// const prompt = require("prompt-sync")()
+
+// const num = prompt("Enter Number :- ")
+
+// let i =1
+
+// while(i<=10){
+//     console.log(num*i)
+//     i++
+// }
+
+// let i = 1
+// do{
+//     console.log(i)
+//     i++
+// }
+
+// while(i<=10)
+
+
+// for(let i = 1; i<=10; i++){
+//     if(i==5){
+//         break
+//     }
+//     if(i==2){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+//5*5 = 25
+// 4*3 = 12
+
+// let a = 7
+// let b = 6
+// while(){
+
+// }
+
+// let x = 9
+
+// for(let i=1; i<=x; i++){
+   
+//     if(x<=10){
+//         console.log("Hy"+i)
+//     }
+//     else if(x<=20){
+//         console.log("By"+i)
+//     }
+// }
+
+
+// // 1-100 / 11
+
+// let i = 1
+
+// while(i<=100){
+//     if(i%11==0){
+//         console.log(i)
+//     }
+//     i++
+// }
+
+// let lastName = "Sharma"
+
+// function init(){
+//     let firstName = "Devanshu"
+//     console.log(lastName)
+// }
+
+
+// // console.log(firstName)
+
+// init()
+
+
+// function init(){
+//     let firstName = "Devanshu"
+//     let lastName = "Sharma"
+//     function outer(){
+//         console.log(firstName)
+        
+//         function outer1(){
+//             console.log(lastName)
+//         }
+//         outer1()
+//     }
+//     outer()
+// }
+
+// init()
+
+
+// function outer(){
+//     let age = 18
+
+//     function inner(){
+//         console.log(age)
+//     }
+
+//     return inner
+// }
+
+
+// const clousre =  outer()
+
+// clousre()
+
+
+
